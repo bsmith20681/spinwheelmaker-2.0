8664f9100efbf9340160f78ab95a4abb17d727b3
Ben Smith <bsmith20681@gmail.com>
2022-04-27 19:51:05 -0400

    second commit

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Layout = (props) => {
+  return (
+    <div className="container">
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pages/create.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout";
+
+const Create = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <h1 className="text-3xl font-bold ">Create a wheel</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default Create;
