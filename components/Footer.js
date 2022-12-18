8275ffd29ffa720845da4008f662f236e0377f1d
Ben Smith <bsmith20681@gmail.com>
2022-12-18 12:49:48 -0500

    finished disclouse component

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,26 @@
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-gray-900">
-      <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
-        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-          <div className="px-5 py-2">
+    <footer className="bg-neutral-800">
+      <div className="mx-auto flex max-w-7xl flex-wrap justify-between overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
+        <p className="text-center text-base text-gray-400">&copy; {year} spinwheelmaker.com. All rights reserved.</p>
+        <nav className="flex flex-wrap items-end justify-center" aria-label="Footer">
+          <div className="px-5">
             <a href="#" className="text-base text-gray-400 hover:text-gray-600">
               About
             </a>
           </div>
-          <div className="px-5 py-2">
+          <div className="px-5">
             <a href="#" className="text-base text-gray-400 hover:text-gray-600">
               Privacy Policy
             </a>
           </div>
-          <div className="px-5 py-2">
+          <div className="px-5">
             <a href="#" className="text-base text-gray-400 hover:text-gray-600">
               Terms and Conditions
             </a>
           </div>
         </nav>
-        <p className="mt-8 text-center text-base text-gray-400">&copy; {year} spinwheelmaker.com. All rights reserved.</p>
       </div>
     </footer>
   );
--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,10 @@ import MysteryIcon from "../public/images/mystery.png";
 import RandomNumberIcon from "../public/images/number.png";
 import GiveAwayIcon from "../public/images/giveaway.png";
 import RandomNamePicker from "../public/images/name.png";
-import TryItOut from "../public/images/try-it-out.png";
 
-import { Disclosure } from "@headlessui/react";
+import { PlusCircleIcon, MinusCircleIcon } from "@heroicons/react/24/outline";
+
+import { Disclosure, Transition } from "@headlessui/react";
 
 export default function Home() {
   return (
@@ -115,10 +116,31 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <Disclosure>
-        <Disclosure.Button className="py-2">Is team pricing available?</Disclosure.Button>
-        <Disclosure.Panel className="text-gray-500">Yes! You can purchase a license that you can share with your entire team.</Disclosure.Panel>
-      </Disclosure>
+      <div className="container">
+        <h2 className="mt-6 mb-3 text-center text-2xl font-bold text-neutral-800">Frequently Asked Questions</h2>
+        <div className="mx-auto max-w-5xl">
+          <Disclosure>
+            {({ open }) => (
+              <div className={open ? "my-6 rounded-md border-2 border-blue-300 bg-blue-100 px-7 py-2" : "my-6 rounded-md border-2 border-gray-300 bg-gray-100 px-7 py-2"}>
+                <Disclosure.Button className={open ? "flex w-full justify-between font-medium text-blue-500" : "flex w-full justify-between font-medium "}>
+                  Why use our Spin wheel maker?
+                  {open ? <MinusCircleIcon className={open ? "w-6" : "w-6"} /> : <PlusCircleIcon className={open ? "w-6" : "w-6"} />}
+                </Disclosure.Button>
+                <Transition
+                  enter="transition duration-300 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                >
+                  <Disclosure.Panel className="mt-4 text-gray-500">Yes! You can purchase a license that you can share with your entire team.</Disclosure.Panel>
+                </Transition>
+              </div>
+            )}
+          </Disclosure>
+        </div>
+      </div>
     </Layout>
   );
 }
